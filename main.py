47a9cbc7c771d133eaa457b9d7a648c2cf5101ba
AlexanderKamynin <kamyninsasa@gmail.com>
2026-01-30 21:28:54 +0300

    add main simulator file

---
+++ main.py
@@ -0,0 +1,122 @@
+import cv2
+import argparse
+import sys
+
+import gym
+import numpy as np
+import pyglet
+from pyglet.window import key
+
+from gym_duckietown.envs import DuckietownEnv
+
+
+CONST_UP_SPEED = 
+
+# python3 main.py --map-name=udem1
+parser = argparse.ArgumentParser()
+parser.add_argument("--env-name", default="Duckietown-udem1-v0")
+parser.add_argument("--map-name", default="udem1")
+parser.add_argument("--distortion", default=False, action="store_true")
+parser.add_argument("--camera_rand", default=False, action="store_true")
+parser.add_argument(
+    "--draw-curve", action="store_true", help="draw the lane following curve"
+)
+parser.add_argument(
+    "--draw-bbox", action="store_true", help="draw collision detection bounding boxes"
+)
+parser.add_argument(
+    "--domain-rand", action="store_true", help="enable domain randomization"
+)
+parser.add_argument(
+    "--dynamics_rand", action="store_true", help="enable dynamics randomization"
+)
+parser.add_argument(
+    "--frame-skip", default=1, type=int, help="number of frames to skip"
+)
+parser.add_argument("--seed", default=42, type=int, help="seed")
+args = parser.parse_args()
+
+if args.env_name and args.env_name.find("Duckietown") != -1:
+    env = DuckietownEnv(
+        seed=args.seed,
+        map_name=args.map_name,
+        draw_curve=args.draw_curve,
+        draw_bbox=args.draw_bbox,
+        domain_rand=args.domain_rand,
+        frame_skip=args.frame_skip,
+        distortion=args.distortion,
+        camera_rand=args.camera_rand,
+        dynamics_rand=args.dynamics_rand,
+    )
+else:
+    env = gym.make(args.env_name)
+
+env.reset()
+env.render()
+
+
+@env.unwrapped.window.event
+def on_key_press(symbol, modifiers):
+    """
+    This handler processes keyboard commands that
+    control the simulation
+    """
+
+    if symbol == key.BACKSPACE or symbol == key.SLASH:
+        print("RESET")
+        env.reset()
+        env.render()
+    elif symbol == key.PAGEUP:
+        env.unwrapped.cam_angle[0] = 0
+    elif symbol == key.ESCAPE:
+        env.close()
+        sys.exit(0)
+
+
+# Register a keyboard handler
+key_handler = key.KeyStateHandler()
+env.unwrapped.window.push_handlers(key_handler)
+
+
+RENDER_PARAMS = ["human", "top_down"]
+
+def update(dt):
+    """
+    This function is called at every frame to handle
+    movement/stepping and redrawing
+    """
+
+    action = np.array([0.0, 0.0])
+
+    if key_handler[key.UP]:
+        # [-1, 1] - |+-1|: максимальная скорость (~0.30м/c)
+        # 1 -> 0 : 0.5 (~ в 2 раза меньше скорость!)
+        action += np.array([0.44, 0.0])
+    if key_handler[key.DOWN]: 
+        action -= np.array([0.44, 0])
+    if key_handler[key.LEFT]:
+        action += np.array([0, 1])
+    if key_handler[key.RIGHT]:
+        action += np.array([0, -1])
+    if key_handler[key.SPACE]:
+        action = np.array([0, 0])
+
+    # Speed boost
+    if key_handler[key.LSHIFT]:
+        action *= 1.5
+
+    obs, reward, done, info = env.step(action)
+    # obs - картинка (в виде трехмерной матрицы)
+    # done = True|False
+    
+    print("step_count = %s, reward=%.3f" % (env.unwrapped.step_count, reward))
+    print("bot position = ", env.cur_pos)
+
+    env.render("human")
+
+pyglet.clock.schedule_interval(update, 1.0 / env.unwrapped.frame_rate)
+
+# Enter main event loop
+pyglet.app.run()
+
+env.close()
